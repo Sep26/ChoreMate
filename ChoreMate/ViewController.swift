8a06382d5ac4347b6641041cf3be82a406582e6d
Sepehr Behmanesh Fard <sepehr_b@berkeley.edu>
2023-12-18 17:14:45 -0800

    Initial Commit

---
+++ ChoreMate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ChoreMate
+//
+//  Created by Sepehr Behmanesh on 12/18/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
